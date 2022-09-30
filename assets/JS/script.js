acca2ca533b7d302b2671a091be1088b194b463c
Erik Costa <105504700+ErikCosta-O@users.noreply.github.com>
2022-09-30 19:25:25 -0300

    Add files via upload

--- assets/JS/script.js
+++
@@ -1,8 +0,0 @@
-function menu(){
-    let menuArea = document.querySelector("#menu-area")
-    if ( menuArea.style.width == '130px'){
-        menuArea.style.width = '0px'
-    }else{
-        menuArea.style.width = '130px'
-    }
-}
